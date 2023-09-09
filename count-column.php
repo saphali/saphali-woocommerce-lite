f917e177bb8a4a459e6eaf49b7229ed422d97f20
Saphali <saphali@ukr.net>
2023-09-09 12:51:22 +0300

    fix

--- count-column.php
+++ count-column.php
@@ -14,6 +14,7 @@ if(isset($_POST['column_count_saphali'])) {
 <form action="" method="POST">
 Количество колонок: <input  value='<?php echo get_option('column_count_saphali'); ?>' type="text" name="column_count_saphali" />
 <div class='clear'></div>
+<input type="hidden" name="_wpnonce" value="<?php echo wp_create_nonce('fields-nonce'); ?>">
 <input type="submit" class="button alignleft" value="Сохранить"/>
 </form> 
-<form action="" method="POST"><input type="hidden" name="column_count_saphali" value="1"/><input type="hidden" name="clear" value="1"/><input type="submit" class="button alignright" value="Сброс на умолчание"/></form> 
+<form action="" method="POST"><input type="hidden" name="_wpnonce" value="<?php echo wp_create_nonce('fields-nonce'); ?>"><input type="hidden" name="column_count_saphali" value="1"/><input type="hidden" name="clear" value="1"/><input type="submit" class="button alignright" value="Сброс на умолчание"/></form> 
